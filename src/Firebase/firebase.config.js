a22a5ee6f0cf0faa9de707a5948b73153c5b6c48
faruk <2035faruk@gmail.com>
2024-12-22 19:27:55 +0600

    Secure Firebase configuration keys using environment variables

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9cR4SYhaJSQhtOPsICZbQGFRrGypnI9Y",
-  authDomain: "antiquify-68162.firebaseapp.com",
-  projectId: "antiquify-68162",
-  storageBucket: "antiquify-68162.firebasestorage.app",
-  messagingSenderId: "1082692149724",
-  appId: "1:1082692149724:web:0c2e06fbcbb7a66fc66f19",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
